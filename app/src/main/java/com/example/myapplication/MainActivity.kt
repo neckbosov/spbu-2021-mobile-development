3690fd6144b439f70e5ca813501f0804836e2c03
Nikita Bosov <neckbosov@gmail.com>
2021-09-22 14:55:38 +0300

    Add simple user list

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.example.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(
+            this,
+            LinearLayoutManager.VERTICAL,
+            false
+        )
+
+
+        val adapter = UserAdapter()
+        recyclerView.adapter = adapter
+        adapter.notifyDataSetChanged()
     }
 }
---
+++ app/src/main/java/com/example/myapplication/User.kt
@@ -0,0 +1,7 @@
+package com.example.myapplication
+
+data class User(
+    val avatarUrl: String,
+    val name: String,
+    val groupName: String
+)
