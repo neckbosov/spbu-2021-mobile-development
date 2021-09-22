e22f4f1e7611fe8c543656779353e82decbef831
Nikita Bosov <neckbosov@gmail.com>
2021-09-22 15:30:21 +0300

    Add web query for getting users

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,25 +2,56 @@ package com.example.myapplication
 
 import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.core.view.isVisible
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.recyclerview.widget.RecyclerView
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
+    val viewModel: MainViewModel by viewModels()
+
     @SuppressLint("NotifyDataSetChanged")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
-        recyclerView.layoutManager = LinearLayoutManager(
-            this,
-            LinearLayoutManager.VERTICAL,
-            false
-        )
+        val userAdapter = setupRecyclerView()
+        val userRecyclerView = findViewById<View>(R.id.recyclerView).also {
+            it.isVisible = false
+        }
+        val progressBar = findViewById<View>(R.id.progressBar).also {
+            it.isVisible = true
+        }
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+                viewModel.viewState.collect {
+                    when (it) {
+                        is MainViewModel.ViewState.Data -> {
+                            userAdapter.userList = it.users
+                            userAdapter.notifyDataSetChanged()
+                            progressBar.isVisible = false
+                            userRecyclerView.isVisible = true
+                        }
+                        is MainViewModel.ViewState.Loading -> {
+                            userAdapter.userList = listOf()
+                            progressBar.isVisible = true
+                            userRecyclerView.isVisible = false
+                        }
+                    }
+                }
+            }
+        }
+    }
 
 
-        val adapter = UserAdapter()
-        recyclerView.adapter = adapter
-        adapter.notifyDataSetChanged()
+    private fun setupRecyclerView(): UserAdapter {
+        val usersRecyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        val userAdapter = UserAdapter()
+        usersRecyclerView.adapter = userAdapter
+        return userAdapter
     }
 }
