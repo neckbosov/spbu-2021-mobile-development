ccb1aea4c2f7b73911f1a48c5dc53a430682366d
Nikita Bosov <neckbosov@gmail.com>
2022-01-04 23:01:08 +0300

    add basic navigation

--- app/src/main/java/com/example/myapplication/ui/MainViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/MainViewModel.kt
@@ -1,5 +1,10 @@
 package com.example.myapplication.ui
 
 import com.example.myapplication.ui.base.BaseViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 
-class MainViewModel : BaseViewModel()
+class MainViewModel : BaseViewModel() {
+    val isAuthorizedFlow: Flow<Boolean> = MutableStateFlow(false)
+
+}
